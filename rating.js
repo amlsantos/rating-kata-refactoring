6c7f997527d8ed30278ed2aefcd316f29cb9bd2e
Andre Santos <6472330+santos-an@users.noreply.github.com>
2024-02-23 21:18:36 +0000

    create rating class

---
+++ rating.js
@@ -0,0 +1,21 @@
+class Rating
+{
+    constructor(voyage, history)
+    {
+        this.voyage = voyage;
+        this.history = history;
+    }
+
+    get value()
+    {
+
+    }
+
+    get voyageRisk() {}
+
+    get captainHistoryRisk() {}
+
+    get voyageProfitFactor() {}
+
+    get hasChinaHistory() {}
+}
